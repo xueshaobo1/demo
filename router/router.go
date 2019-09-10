e3a7efaf9a07189335d084c12fa140bb4814cdd2
xueshaobo1 <38201863+xueshaobo1@users.noreply.github.com>
2019-09-10 09:11:07 +0800

    路由优化

--- router/router.go
+++ router/router.go
@@ -21,8 +21,23 @@ func Run(){
     //添加中间件
     api := e.Group(`/api`, midJwt)
     api.GET("/sys",controllers.Index)
-
-
+    /*
+    组可以单独写在其他配置文件中
+    例如：
+    router.go //写在router.go文件中
+    api := e.Group(`/api`, midJwt)
+    Api(api)
+    
+    api.go //写在api.go文件中
+    package router
+    import (
+        "github.com/labstack/echo"
+        "demo/controllers"
+     )
+    func Api(api *echo.Group){
+    api.GET("/sys",controllers.Index)
+    这样可以把路由拆开卸载不同的文件里面
+    */
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
     e.Logger.Fatal(e.Start(":1323"))
